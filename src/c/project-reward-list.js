aba46c6c0fb8353d428d94d7d2f822d01f24bb04
Rafael Simon Maia <rafaelsimonmaia@gmail.com>
2016-04-28 21:18:58 -0600

    Add lots of translations and fixed the tests when needed.

--- src/c/project-reward-list.js
+++ src/c/project-reward-list.js
@@ -9,27 +9,27 @@ window.c.ProjectRewardList = ((m, h, _) => {
 
                 return m('a[class="' + (h.rewardSouldOut(reward) ? 'card-gone' : 'card-reward ' + (project().open_for_contributions ? 'clickable' : '')) + ' card card-secondary u-marginbottom-10"][href="' + (project().open_for_contributions && !h.rewardSouldOut(reward) ? contributionUrlWithReward : 'js:void(0);') + '"]', [
                     m('.u-marginbottom-20', [
-                        m('.fontsize-base.fontweight-semibold', 'Para R$ ' + h.formatNumber(reward.minimum_value) + ' ou mais'),
-                        m('.fontsize-smaller.fontweight-semibold', h.pluralize(reward.paid_count, ' apoio', ' apoios')), (reward.maximum_contributions > 0 ? [
+                        m('.fontsize-base.fontweight-semibold', 'For $ ' + h.formatNumber(reward.minimum_value) + ' or more'),
+                        m('.fontsize-smaller.fontweight-semibold', h.pluralize(reward.paid_count, ' contribution', ' contributions')), (reward.maximum_contributions > 0 ? [
                             (reward.waiting_payment_count > 0 ? m('.maximum_contributions.in_time_to_confirm.clearfix', [
-                                m('.pending.fontsize-smallest.fontcolor-secondary', h.pluralize(reward.waiting_payment_count, ' apoio em prazo de confirmação', ' apoios em prazo de confirmação.'))
+                                m('.pending.fontsize-smallest.fontcolor-secondary', h.pluralize(reward.waiting_payment_count, ' contribution awaiting confirmation', ' contributions awaiting confirmation'))
                             ]) : ''), (h.rewardSouldOut(reward) ? m('.u-margintop-10', [
-                                m('span.badge.badge-gone.fontsize-smaller', 'Esgotada')
+                                m('span.badge.badge-gone.fontsize-smaller', 'Out of stock')
                             ]) : m('.u-margintop-10', [
                                 m('span.badge.badge-attention.fontsize-smaller', [
-                                    m('span.fontweight-bold', 'Limitada'),
-                                    ' (' + h.rewardRemaning(reward) + ' de ' + reward.maximum_contributions + ' disponíveis)'
+                                    m('span.fontweight-bold', 'Limited'),
+                                    ' (' + h.rewardRemaning(reward) + ' of ' + reward.maximum_contributions + ' available)'
                                 ])
                             ]))
                         ] : ''),
                     ]),
                     m('.fontsize-smaller.u-margintop-20', m.trust(h.simpleFormat(reward.description))), (!_.isEmpty(reward.deliver_at) ?
                         m('.fontsize-smaller', [
-                            m('b', 'Estimativa de Entrega: '),
+                            m('b', 'Expected delivery date: '),
                             h.momentify(reward.deliver_at, 'MMM/YYYY')
                         ]) : ''), (project().open_for_contributions && !h.rewardSouldOut(reward) ?
                         m('.project-reward-box-hover', [
-                            m('.project-reward-box-select-text.u-text-center', 'Selecione essa recompensa')
+                            m('.project-reward-box-select-text.u-text-center', 'Select your reward')
                         ]) : '')
                 ]);
             }));
